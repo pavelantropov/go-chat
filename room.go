6635264fed393359d2e51f1dd7c1f3bc0bd15c1d
Pavel Antropov <p.m.antropov@gmail.com>
2026-01-05 10:49:26 +0400

    update room

--- room.go
+++ room.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
 type room struct {
 	clients map[*client]bool
 
@@ -7,4 +14,61 @@ type room struct {
 	leave chan *client
 
 	forward chan []byte
+}
+
+func newRoom() *room {
+	return &room{
+		clients: make(map[*client]bool),
+		join:    make(chan *client),
+		leave:   make(chan *client),
+		forward: make(chan []byte),
+	}
+}
+
+func (r *room) run() {
+	for {
+		select {
+		case client := <-r.join:
+			r.clients[client] = true
+		case client := <-r.leave:
+			delete(r.clients, client)
+			close(client.receive)
+		case msg := <-r.forward:
+			for client := range r.clients {
+				client.receive <- msg
+			}
+		}
+	}
+}
+
+const (
+	socketBufferSize  = 1024
+	messageBufferSize = 256
+)
+
+var upgrader = &websocket.Upgrader{
+	ReadBufferSize:  socketBufferSize,
+	WriteBufferSize: messageBufferSize,
+}
+
+func (r *room) ServerHttp(w http.ResponseWriter, req *http.Request) {
+	socket, err := upgrader.Upgrade(w, req, nil)
+
+	if err != nil {
+		log.Fatalln("ServerHTTP: ", err)
+		return
+	}
+
+	client := &client{
+		socket:  socket,
+		receive: make(chan []byte, messageBufferSize),
+		room:    r,
+	}
+
+	r.join <- client
+
+	defer func() { r.leave <- client }()
+
+	go client.write()
+	client.read()
 }
