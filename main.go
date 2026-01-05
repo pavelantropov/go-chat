948f7bd0adf5106a4cdef096af8df89e34b0637c
Pavel Antropov <p.m.antropov@gmail.com>
2026-01-05 10:57:56 +0400

    update main

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"log"
+	"net/http"
+)
 
+func main() {
+	var addr = flag.String("addr", ":8080", "App address")
+	flag.Parse()
+	r := newRoom()
+
+	http.Handle("/room", r)
+
+	go r.run()
+
+	log.Println("Starting web server on", *addr)
+
+	if err := http.ListenAndServe(*addr, nil); err != nil {
+		log.Fatalln("ListenAndServe:", err)
+		return
+	}
 }
